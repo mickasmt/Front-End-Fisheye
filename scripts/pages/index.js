6b2e74b04d575cc31d76ee2a8f010e5da00c7b0f
Mickael Sermont <sermont.mickael@hotmail.fr>
2022-05-11 12:10:46 +0200

    feat: update getPhotographers() & assets folder

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,59 +1,30 @@
-async function getPhotographers() {
-    let photographers = [];
-    let pathFileData = "./data/photographers.json";
-    
-    // Fetch data in json file and return all photographers
-    fetch(pathFileData)
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            photographers = data.photographers;
-            console.log(photographers);
+    async function getPhotographers() {
+        const pathFileData = "./data/photographers.json";
+
+        // fetch data in file json and return
+        try {
+            let res = await fetch(pathFileData);
+            return await res.json();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async function displayData(photographers) {
+        const photographersSection = document.querySelector(".photographer_section");
+
+        photographers.forEach((photographer) => {
+            const photographerModel = photographerFactory(photographer);
+            const userCardDOM = photographerModel.getUserCardDOM();
+            photographersSection.appendChild(userCardDOM);
         });
+    };
 
-
-    // const photographers = [
-    //     {
-    //         "name": "Ma data test",
-    //         "id": 1,
-    //         "city": "Paris",
-    //         "country": "France",
-    //         "tagline": "Ceci est ma data test",
-    //         "price": 400,
-    //         "portrait": "account.png"
-    //     },
-    //     {
-    //         city: "London"
-            // country: "UK"
-            // id: 243
-            // name: "Mimi Keel"
-            // portrait: "MimiKeel.jpg"
-            // price: 400
-            // tagline: "Voir le beau dans le quotidien"
-    //     },
-    // ]
-    // et bien retourner le tableau photographers seulement une fois
-    return ({
-        photographers: photographers
-    })
-}
-
-async function displayData(photographers) {
-    const photographersSection = document.querySelector(".photographer_section");
-
-    photographers.forEach((photographer) => {
-        const photographerModel = photographerFactory(photographer);
-        const userCardDOM = photographerModel.getUserCardDOM();
-        photographersSection.appendChild(userCardDOM);
-    });
-};
-
-async function init() {
-    // Récupère les datas des photographes
-    const { photographers } = await getPhotographers();
-    displayData(photographers);
-};
-
-init();
+    async function init() {
+        // Récupère les datas des photographes
+        const { photographers } = await getPhotographers();
+        displayData(photographers);
+    };
+    
+    init();
     
