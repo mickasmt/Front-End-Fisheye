864ac2f80fe2697460f375a53360df9a95dc53ee
Mickael Sermont <sermont.mickael@hotmail.fr>
2022-06-07 16:34:19 +0200

    feat: show log formData after submit form

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -7,3 +7,21 @@ function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
 }
+
+
+var contactForm = document.getElementById('contactForm');
+
+// validation form event: SUBMIT
+contactForm.addEventListener("submit", formValidation);
+
+/** Manage validation form */
+function formValidation(e) {
+    e.preventDefault();
+    
+    let formData = new FormData(contactForm);
+    
+    // Log the key/value pairs
+    for (var pair of formData.entries()) {
+        console.log(pair[0]+ ' - ' + pair[1]); 
+    }
+}
