31ac7d7021a080a1326b8bc5d1335fba15f1388c
Mickael Sermont <sermont.mickael@hotmail.fr>
2022-06-14 14:05:17 +0200

    debug: index page data

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,4 +1,4 @@
-import { photographerFactory } from "../factories/photographer";
+import { photographerFactory } from "../factories/photographer.js";
 
 async function getPhotographers() {
     // for ONLY github pages - not work on local
